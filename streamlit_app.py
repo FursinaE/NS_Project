d578922e8c96b56801a9fd2dc1e7341e980c83cf
Ekaterina Fursina <fursinae@gmail.com>
2024-12-16 10:49:30 +0100

    Updated periods to 500

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ else:
     m = Prophet()
     m.fit(df_timeseries.rename(columns={"start_time": "ds", "nb_disruptions": "y"}))
 
-    future = m.make_future_dataframe(periods=376, freq="d")
+    future = m.make_future_dataframe(periods=500, freq="d")
 
     forecast = m.predict(future)
 
